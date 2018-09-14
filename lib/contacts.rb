3cc8fc4a4323781011e8bbff5aaebde65defb3aa
alice T <alicewtien@gmail.com>
2018-09-14 18:37:36 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data| #attributre = key, data = value
       if attribute == favorite_icea_cream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
         #delete_if - iterates over data (flavors)
       end
     end
